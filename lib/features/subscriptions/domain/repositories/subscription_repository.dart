1d56aabe90584c651420a359bfd7d66e024bc6fc
davexdev <xdave418@gmail.com>
2026-05-12 14:26:15 -0600

    feat(domain): add SubscriptionRepository abstract interface

---
+++ lib/features/subscriptions/domain/repositories/subscription_repository.dart
@@ -0,0 +1,23 @@
+import 'package:subscan/features/subscriptions/models/subscription.dart';
+
+/// Interfaz para acceder a datos de suscripciones
+/// Implementada por data layer
+abstract class SubscriptionRepository {
+  /// Obtiene todas las suscripciones del usuario
+  Future<List<Subscription>> getSubscriptions();
+
+  /// Obtiene una suscripción por ID
+  Future<Subscription?> getSubscriptionById(String id);
+
+  /// Guarda una nueva suscripción
+  Future<void> saveSubscription(Subscription subscription);
+
+  /// Actualiza una suscripción existente
+  Future<void> updateSubscription(Subscription subscription);
+
+  /// Elimina una suscripción por ID
+  Future<void> deleteSubscription(String id);
+
+  /// Sincroniza con Gmail (obtiene nuevas suscripciones)
+  Future<List<Subscription>> syncWithGmail();
+}
